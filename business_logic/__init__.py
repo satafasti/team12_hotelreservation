847865b7053fdf05081e4c75f0aae7d1ca3396ad
grabs2 <sarinag_official@hotmail.com>
2025-04-27 20:01:27 +0200

    Erstellung __init__.py

--- business_logic/__init__.py
+++ business_logic/__init__.py
@@ -1,9 +1,9 @@
 #from .adress_manager import
-from .booking_manager import
-from .facilities_manager import
-from .guest_manager import
-from .hotel_manager import
+from .booking_manager import Booking_Manager
+from .facilities_manager import Facilities_Manager
+from .guest_manager import Guets_Manager
+from .hotel_manager import Hotel_Manager
 #from .invoice_manager import
-#from .room_facilities_manager import
+#from .room_facilities_manager import #Benötigen wir das? ist doch nur Zwischentable in SQL?
 from .room_manager import Room_Manager
 #from .room_type_manager import
