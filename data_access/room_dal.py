1e52fce20625bbffb88836b199ab627c234b5115
grabs2 <sarinag_official@hotmail.com>
2025-04-27 20:02:09 +0200

    Vorbereiten Data Access

--- data_access/room_dal.py
+++ data_access/room_dal.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
+import model
+from data_access.base_dal import BaseDal
+
+
 class RoomDAL(BaseDal):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
