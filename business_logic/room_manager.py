e0b1ad27a39ab50b22632ef78e479b7adc69d3e4
grabs2 <sarinag_official@hotmail.com>
2025-04-27 20:02:45 +0200

    Anpassung facilities, room und room_type

--- business_logic/room_manager.py
+++ business_logic/room_manager.py
@@ -1,4 +1,4 @@
-import os
+#import os
 
 import model
 import data_access
@@ -7,14 +7,17 @@ class Room_Manager():
     def __init__(self) -> None:
         self.__room_dal = data_access.RoomDAL()
 
-    def create_room(self, room_id : int, room_number : int, price_per_night : float, type_id : model.room_type) -> model.Room:
-        return self.__room_dal.create_new_album(title, artist)
+    def create_room(self, room_id : int, room_number : int, price_per_night : float, description : model.Room_Type) -> model.Room:
+        return self.__room_dal.create_new_room(room_id, room_number, price_per_night, description)
 
-    def read_artists_albums(self, artist: model.Artist) -> None:
-        self.__album_dal.read_albums_by_artist(artist)
+    def get_room_details(self, room: Room):
+        return f"Room ID: {self.room_id}, Room Number: {self.room_number}, Price per Night: {self.price_per_night}, Room Type: {self.room.description}"
 
-    def read_album(self, album_id: int) -> model.Album:
-        return self.__album_dal.read_album_by_id(album_id)
+    #def read_facilities_room(self, room: model.facilities) -> None:
+        #self.__room_dal.read_facilities_by_room(facilities)
+
+    #def read_room(self, room_id : int, room_number : int, price_per_night : float, type_id : model.room_type) -> model.Room:
+        #return self.__room_dal.read_room_by_id(room_id)
 
 #class Album_Manager():
     #def __init__(self) -> None:
