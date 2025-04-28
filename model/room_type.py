f9890e842fe27ea51632d3582e0f217c0b521d5e
grabs2 <sarinag_official@hotmail.com>
2025-04-28 22:07:21 +0200

    if not Bedingungen ergänzt

--- model/room_type.py
+++ model/room_type.py
@@ -1,6 +1,19 @@
 class Room_Type:
 
     def __init__(self, type_id : int, description : str, max_guests : int):
+        if not type_id:
+            raise ValueError("type_id must be set")
+        if not isinstance(type_id, int):
+            raise TypeError("type_id must be a int")
+        if not description:
+            raise ValueError("description must be set")
+        if not isinstance(description, str):
+            raise TypeError("description must be a str")
+        if not max_guests:
+            raise ValueError("max_guests must be set")
+        if not isinstance(max_guests, int):
+            raise TypeError("max_guests must be a int")
+
         self.__type_id = type_id
         self.__description = description #z.B. Einzelzimmer
         self.__max_guests = max_guests
