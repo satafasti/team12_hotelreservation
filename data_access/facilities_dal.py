648f5b870cf8be710a69bc4aa34259093c0d9363
grabs2 <sarinag_official@hotmail.com>
2025-05-15 13:19:52 +0200

    Syntaxfehler behoben

--- data_access/facilities_dal.py
+++ data_access/facilities_dal.py
@@ -7,3 +7,39 @@ from data_access.base_dal import Base_DAL
 class FacilitiesDAL(Base_DAL):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
+
+
+    def create_facilities(self, facilities: model.Facilities):
+        sql = """
+        INSERT INTO Facilities (facility_id, facility_name) VALUES (?, ?)
+        """
+        params = (facilities.facility_id if facilities else None, facilities.facility_name)
+        self.execute(sql, params)
+
+    def show_facilities_by_id(self, facilities: model.Facilities):
+        sql = """
+        SELECT * FROM Facilities WHERE facility_id = ?
+        """
+        params = (facilities.facility_id,)
+        result = self.fetch_one(sql, params)
+        if result:
+            facility_id, facility_name = result
+            return model.Facilities(facility_id, facility_name)
+        else:
+            return None
+
+    def update_facilities(self, facilities: model.Facilities):
+        sql = """
+        UPDATE Facilities SET facility_name = ? WHERE facility_id = ?
+        """
+        params = (facilities.facility_name,facilities.facility_id)
+        self.execute(sql, params)
+
+    def delete_facilities(self, facilities: model.Facilities):
+        sql = """
+        DELETE FROM Facilities WHERE facility_id = ?
+        """
+        params = (facilities.facility_id,)
+        self.execute(sql, params)
+
+
