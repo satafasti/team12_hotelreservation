1049d24c92dd2a748b8abf2b3ff922e3193db91b
grabs2 <sarinag_official@hotmail.com>
2025-05-15 13:10:59 +0200

    Syntaxfehler behoben

--- data_access/room_type_dal.py
+++ data_access/room_type_dal.py
@@ -1,7 +1,7 @@
 import model
 from data_access.base_dal import Base_DAL
 
-class RoomFacilitiesDAL(Base_DAL):
+class RoomTypeDAL(Base_DAL):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
 
