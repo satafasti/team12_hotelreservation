f3d68aa26f4f655916fee998a0d5c6922f9ab7d9
Fabia Holzer <holzerpamina@gmail.com>
2025-05-23 13:17:01 +0200

    room Ui erfasst

---
+++ ui_folder/room_ui.py
@@ -0,0 +1,3 @@
+#2. Als Gast möchte ich Details zu verschiedenen Zimmertypen (Single, Double, Suite usw.), die in einem Hotel verfügbar sind, sehen, einschliesslich der maximalen Anzahl von Gästen für dieses Zimmer, Beschreibung, Preis und Ausstattung, um eine fundierte Entscheidung zu treffen.
+#2.1. Ich möchte die folgenden Informationen pro Zimmer sehen: Zimmertyp, max. Anzahl der Gäste, Beschreibung, Ausstattung, Preis pro Nacht und Gesamtpreis.
+#2.2. Ich möchte nur die verfügbaren Zimmer sehen, sofern ich meinen Aufenthalt (von – bis) spezifiziert habe.
