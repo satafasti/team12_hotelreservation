58b9a859cbaa0e77efdf86f291e902b90a7a015d
grabs2 <sarinag_official@hotmail.com>
2025-05-23 14:08:50 +0200

    show_all_hotels return Enrückung angepasst

--- data_access/hotel_dal.py
+++ data_access/hotel_dal.py
@@ -11,7 +11,7 @@ class HotelDAL(BaseDAL):
 
     def create_hotel(self, hotel: model.Hotel):
         sql = """
-        INSERT INTO Hotel(hotel_id, name, stars, address_id) VALUES (?, ?, ?, ?) #address_id kommt aber von address
+        INSERT INTO Hotel(hotel_id, name, stars, address_id) VALUES (?, ?, ?, ?)
         """
         params = (hotel.hotel_id if hotel else None, hotel.name, hotel.stars, hotel.address_id)
         self.execute(sql, params)
@@ -26,7 +26,7 @@ class HotelDAL(BaseDAL):
             hotel_id, name, stars, address_id = row
             hotels.append(model.Hotel(hotel_id, name, stars, address_id))
 
-            return hotels
+    return hotels
 
     def update_hotel(self, hotel: model.Hotel):
         sql = """
