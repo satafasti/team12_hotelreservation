bf15e09556dd4cea8a5305bde833bac00887c3e0
grabs2 <sarinag_official@hotmail.com>
2025-05-23 12:51:29 +0200

    Tablename angepasst

--- data_access/guest_dal.py
+++ data_access/guest_dal.py
@@ -10,14 +10,14 @@ class GuestDAL(BaseDAL):
 
     def create_guest(self, guest: model.Guest):
         sql = """
-        INSERT INTO Guests(guest_id, first_name, last_name, email, address_id) VALUES (?, ?, ?, ?, ?) #address_id kommt aber von address
+        INSERT INTO Guest(guest_id, first_name, last_name, email, address_id) VALUES (?, ?, ?, ?, ?) 
         """
         params = (guest.guest_id if guest else None, guest.first_name, guest.last_name, guest.email, guest.address_id)
         self.execute(sql, params)
 
     def show_guest_by_id(self, guest: model.Guest):
         sql = """
-        SELECT * FROM Guests WHERE guest_id = ?
+        SELECT * FROM Guest WHERE guest_id = ?
         """
         params = (guest.guest_id,)
         result = self.fetch_one(sql, params)
@@ -29,14 +29,14 @@ class GuestDAL(BaseDAL):
 
     def update_guest(self, guest: model.Guest):
         sql = """
-        UPDATE Guests SET first_name = ? AND last_name = ? WHERE guest_id = ?
+        UPDATE Guest SET first_name = ? AND last_name = ? WHERE guest_id = ?
         """
         params = (guest.first_name, guest.last_name, guest.guest_id)
         self.execute(sql, params)
 
     def delete_guest(self, guest: model.Guest):
         sql = """
-        DELETE FROM Guests WHERE guest_id = ?
+        DELETE FROM Guest WHERE guest_id = ?
         """
         params = (guest.guest_id,)
         self.execute(sql, params)
